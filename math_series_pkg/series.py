2b023dc581f93c94f1cf82b1979645a28f5aba94
JohnnyBackus <john.p.backus@gmail.com>
2024-01-09 17:38:37 -0800

    all tests passed for sumseries function

--- math_series_pkg/series.py
+++ math_series_pkg/series.py
@@ -13,3 +13,11 @@ def lucas (n):
         return 1
     else:
         return lucas (n - 1) + lucas (n - 2)
+    
+def sum_series (n, param1 = 0, param2 = 1):
+    if n == 0:
+        return param1
+    elif n == 1:
+        return param2
+    else:
+        return sum_series (n - 1, param1, param2) + sum_series (n - 2, param1, param2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,24 +1,22 @@
 import pytest
-from math_series_pkg.series import fibonacci, lucas
+from math_series_pkg.series import fibonacci, lucas, sum_series
 
+# -------- Fibonacci tests --------
 
 def test_fibonacci_zero():
     actual = fibonacci(0)
     expected = 0
     assert actual == expected
 
-
 def test_fibonacci_one():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
 
-
 def test_fibonacci_two():
     actual = fibonacci(2)
     expected = 1
     assert actual == expected
-
 
 def test_fibonacci_three():
     actual = fibonacci(3)
@@ -35,7 +33,8 @@ def test_fibonacci_five():
     expected = 5
     assert actual == expected
 
-
+# -------- Lucas tests ----------
+    
 def test_lucas_zero():
     actual = lucas(0)
     expected = 2
@@ -66,7 +65,64 @@ def test_lucas_five():
     expected = 11
     assert actual == expected
 
-def test_lucas_six():
-    actual = lucas(6)
-    expected = 18
+# -------- sum tests --------
+
+def test_sum_series_zero_default():
+    actual = sum_series(0)
+    expected = 0
+    assert actual == expected
+
+def test_sum_series_one_default():
+    actual = sum_series(1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_two_default():
+    actual = sum_series(2)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_three_default():
+    actual = sum_series(3)
+    expected = 2
+    assert actual == expected
+
+def test_sum_series_four_default():
+    actual = sum_series(4)
+    expected = 3
+    assert actual == expected
+
+def test_sum_series_five_default():
+    actual = sum_series(5)
+    expected = 5
+    assert actual == expected
+
+def test_sum_series_zero_params():
+    actual = sum_series(0, 2, 1)
+    expected = 2
+    assert actual == expected
+
+def test_sum_series_one_params():
+    actual = sum_series(1, 2, 1)
+    expected = 1
+    assert actual == expected
+
+def test_sum_series_two_params():
+    actual = sum_series(2, 2, 1)
+    expected = 3
+    assert actual == expected
+
+def test_sum_series_three_params():
+    actual = sum_series(3, 2, 1)
+    expected = 4
+    assert actual == expected
+
+def test_sum_series_four_params():
+    actual = sum_series(4, 2, 1)
+    expected = 7
+    assert actual == expected
+
+def test_sum_series_five_params():
+    actual = sum_series(5, 2, 1)
+    expected = 11
     assert actual == expected
